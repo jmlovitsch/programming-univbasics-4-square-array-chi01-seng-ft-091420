f6d7b0aaa6c9c207536e9b2e3df02417baa06130
John Lovitsch <jmlovitsch@gmail.com>
2020-08-31 21:38:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  new_array = []
-  array.length.times do |index|
-  new_array.push(array[index] ** 2)
+new_array = []
+array.length.times do |index|
+  new_array.push(array[index] + 1)
 end
 new_array
